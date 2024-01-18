caece4418b3cea4e48d0c39e1db4ddf1901a2e97
Gagan Khandate <gagank@cs.columbia.edu>
2024-01-18 01:12:55 +0000

    Functional Nav2D collect_dataset.py

--- nav2d/nav2d.py
+++ nav2d/nav2d.py
@@ -8,13 +8,13 @@ class Map2D:
 
     def __init__(self) -> None:
         
-        self.arena_radius = 1
+        self.arena_radius = 1.5
         self.arena_center = (0, 0)
 
-        self.obstacle_radius = 0.01
-        self.obstacle_center_radius = 0.5
+        self.obstacle_radius = 0.2
+        self.obstacle_center_radius = 0.7
         
-        self.obstacle_angles = np.array([0, 90, 180, 270, 360])
+        self.obstacle_angles = np.array([45, 135, -45, -135])
 
 
 
@@ -27,7 +27,7 @@ class RRTExpert:
         self.goal = None
 
         self.goal_reached = False
-        self.edge_length = 0.05
+        self.edge_length = 0.1
 
 
     def plan(self, start, goal):
@@ -60,8 +60,8 @@ class RRTExpert:
 
         # Check if the new node is valid
         if self._is_valid_node(p_new):
-            np.vstack([self.nodes, p_new])
-            np.vstack([self.edges, p_new - p_near])
+            self.nodes = np.vstack([self.nodes, p_new])
+            self.edges = np.vstack([self.edges, p_new - p_near])
             self.parents.append(np.argmin(np.linalg.norm(self.nodes - p_near, axis=1)))
 
 
@@ -80,7 +80,7 @@ class RRTExpert:
     
     def _find_new_node(self, p_near, p_rand):
         # Find the new node
-        edge = self.edges_length * (p_rand - p_near) / np.linalg.norm(p_rand - p_near)
+        edge = self.edge_length * (p_rand - p_near) / np.linalg.norm(p_rand - p_near)
         p_new = p_near + edge
         return p_new
     
@@ -90,7 +90,8 @@ class RRTExpert:
         if np.linalg.norm(p_new) > self.map.arena_radius:
             return False
         for angle in self.map.obstacle_angles:
-            if np.linalg.norm(p_new - self.map.obstacle_center(angle)) < self.map.obstacle_radius:
+            obstacle_center = self.map.obstacle_center_radius * np.array([ np.cos(np.deg2rad(angle)), np.sin(np.deg2rad(angle))])
+            if np.linalg.norm(p_new - obstacle_center) < self.map.obstacle_radius:
                 return False
         return True
     
@@ -111,18 +112,23 @@ class RRTExpert:
         path.append(self.start)
         path.reverse()
         path = np.array(path)
-        path_edges = self.path[1:] - self.path[:-1]
-        return path, path_edges
-    
+        path_edges = path[1:] - path[:-1]
+        return path, path_edges    
 
-
-def main():
+def single_demo_test():
 
     map = Map2D()
     expert = RRTExpert(map)
 
-    start = np.array([0.5, 0.5])
-    goal = np.array([-0.5, -0.5])
+    start = np.array([0.5, 0.0])
+    goal = np.array([-0.5, 0.0])
+
+    # random start and goal
+    while True:
+        start = np.random.uniform(-map.arena_radius, map.arena_radius, size=2)
+        goal = np.random.uniform(-map.arena_radius, map.arena_radius, size=2)
+        if expert._is_valid_node(start) and expert._is_valid_node(goal):
+            break
 
     expert.plan(start, goal)
 
@@ -133,7 +139,22 @@ def main():
 
     # Plot path
     plt.plot(path[:, 0], path[:, 1], color='yellow')
-    plt.fill_between(path[:, 0], path[:, 1], color='yellow')
+    # plt.fill_between(path[:, 0], path[:, 1], color='yellow')
+
+    # Plot arena
+    arena = plt.Circle(map.arena_center, map.arena_radius, color='green')
+    plt.gcf().gca().add_artist(arena)
+
+    # Plot obstacles
+    for angle in map.obstacle_angles:
+        obstacle_center = map.obstacle_center_radius * np.array([ np.cos(np.deg2rad(angle)), np.sin(np.deg2rad(angle))])
+        obstacle = plt.Circle(obstacle_center, map.obstacle_radius, color='black')
+        plt.gcf().gca().add_artist(obstacle)
+
+    # Plot start and goal
+    plt.plot(start[0], start[1], 'o', color='red')
+    plt.plot(goal[0], goal[1], 'o', color='blue')
+    
 
     # Set plot limits and aspect ratio
     plt.xlim(-map.arena_radius, map.arena_radius)
@@ -144,7 +165,23 @@ def main():
     plt.show()
 
 
+def collect_demo_dataset(num_demo=1000):
+    map = Map2D()
+    expert = RRTExpert(map)
+
+    for i in range(num_demo):
+        angles = np.random.uniform(-180, 180, size=2)
+        radius = np.random.uniform(0, map.arena_radius, size=2)
+        start = radius[0] * np.array([ np.cos(np.deg2rad(angles[0])), np.sin(np.deg2rad(angles[0]))])
+        goal = radius[1] * np.array([ np.cos(np.deg2rad(angles[1])), np.sin(np.deg2rad(angles[1]))])
+        if expert._is_valid_node(start) and expert._is_valid_node(goal):
+            expert.plan(start, goal)
+            path, path_edges = expert._get_path()
+            print(path)
+            print(path_edges)
+            print('----------------------')
+
 if __name__ == '__main__':
-    main()
-    
+
+    single_demo_test()
 
