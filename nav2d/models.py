c10b791912c05f3d9ddce4d5fcac24e44a10f0c0
Gagan Khandate <gagank@cs.columbia.edu>
2024-01-19 17:18:56 +0000

    Training action remmapping network, wandb logging

--- nav2d/models.py
+++ nav2d/models.py
@@ -34,10 +34,23 @@ class OIL(nn.Module):
         self.input_dim = obs_dim
         self.output_dim = act_dim
         self.forward_net = MLP(obs_dim+act_dim, obs_dim, hidden_dim, num_hidden)
-        self.action_net = MLP(obs_dim, act_dim, hidden_dim, num_hidden)
+        self.latent_action_net = MLP(obs_dim, act_dim, hidden_dim, num_hidden)
+        self.action_net = MLP(act_dim, act_dim, hidden_dim, num_hidden)
+        # NOTE: It is not necessary that the dimension of latent action is same as action. 
+        #      You can use a different dimension for latent action and action. 
 
     def forward(self, x):
-        a = self.action_net(x)
-        x = torch.cat([x, a], dim=-1)
+        # Compute latent action
+        z = self.latent_action_net(x)
+        x = torch.cat([x, z], dim=-1)
+
+        # Compute next observation based on latent action and current observation
         y = self.forward_net(x)
+
+        # Compute true action from latent action
+        # NOTE: Detach latent action from the computation graph to avoid backpropagating 
+        # through the action network. Currently we are interested only in understanding if 
+        # the latent action can be used to the predict the the true action.
+        a = self.action_net(z.detach())
+
         return y, a
--- nav2d/train.py
+++ nav2d/train.py
@@ -7,12 +7,18 @@ from models import OIL
 from torch import optim
 from torch import nn
 
+import wandb
 
-def main():
+def wandb_init():
+    wandb.init(project="CPT", tags="nav2d", name="OIL")
+
+
+def train():
     # Create model and optimizer
     model = OIL(obs_dim=4, act_dim=2, hidden_dim=64, num_hidden=2)
     model = model.cuda()
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    optimizer_oil = optim.Adam(model.parameters(), lr=1e-3)
+    optimizer_action = optim.Adam(model.parameters(), lr=1e-3)
     mse = nn.MSELoss()
 
     # Train model
@@ -24,13 +30,31 @@ def main():
             curr_obs = batch_obs[:, 0, :]
             next_obs = batch_obs[:, 1, :]
             next_obs_pred, actions = model(curr_obs)
-            loss = mse(next_obs_pred, next_obs)
-            if i == 0 and epoch == 0:
-                print(f'Epoch: {epoch}, Loss: {loss.item()}')
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            print(f'Epoch: {epoch}, Loss: {loss.item()}')
+
+            # OIL loss
+            loss_oil = mse(next_obs_pred, next_obs)
+            optimizer_oil.zero_grad()
+            loss_oil.backward()
+            optimizer_oil.step()
+            
+
+            # Action prediction loss
+            loss_action = mse(actions, batch_actions)
+            optimizer_action.zero_grad()
+            loss_action.backward()
+            optimizer_action.step()
+            
+            # Log loss
+            print(f'Epoch: {epoch}, OIL loss: {loss_oil.item()}, Action loss: {loss_action.item()}')
+            wandb.log({'OIL loss': loss_oil.item(), 'Action loss': loss_action.item()})
+
+    # TODO: Measure mutual information between 
+            
+
+def main():
+    wandb_init()
+    train()
+    wandb.finish()
 
     
 if __name__ == '__main__':
