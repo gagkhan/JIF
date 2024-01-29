175418b9c02d7f3836e5afa55a32d7dd6754ebbe
Gagan Khandate <gagank@cs.columbia.edu>
2024-01-29 16:06:00 +0000

    Implement action chunking (without VI)

--- nav2d/train.py
+++ nav2d/train.py
@@ -26,10 +26,12 @@ def train(args):
     latent_dim = args.latent_dim
     lr = args.lr
     max_epochs = args.epochs
+    K = args.K
+    seq_len = K + 1
 
     # Create dataloader
     dataloader = Nav2DDataloader(
-        seq_len=2,
+        seq_len=seq_len,
         batch_size=batch_size,
         shuffle=True,
         num_workers=num_workers,
@@ -43,10 +45,10 @@ def train(args):
         num_hidden=2,
         latent_dim=latent_dim,
         detach_latent=detach_latent,
+        action_chunck=K,
     )
     model = model.cuda()
-    optimizer_oil = optim.Adam(model.parameters(), lr=lr)
-    optimizer_action = optim.Adam(model.parameters(), lr=lr)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
     mse = nn.MSELoss()
 
     # Train model
@@ -55,9 +57,12 @@ def train(args):
         for i, batch in enumerate(dataloader):
             batch = [b.to(dtype=torch.float32, device="cuda") for b in batch]
             batch_obs, batch_actions = batch
-            batch_actions = batch_actions[:, 0, :]  # TODO: Fix this hack
+            batch_actions = batch_actions[:, :K, :]  # TODO: Fix this hack
+
             curr_obs = batch_obs[:, 0, :]
-            next_obs = batch_obs[:, 1, :]
+
+            # observation after K steps
+            next_obs = batch_obs[:, K, :]
 
             next_obs_pred, actions = model(curr_obs)
 
@@ -65,9 +70,9 @@ def train(args):
             obs_pred_loss = mse(next_obs_pred, next_obs)
             action_pred_loss = mse(actions, batch_actions)
             loss_oil = obs_pred_loss + action_pred_loss
-            optimizer_oil.zero_grad()
+            optimizer.zero_grad()
             loss_oil.backward()
-            optimizer_oil.step()
+            optimizer.step()
 
             # Log loss
             print(
@@ -100,5 +105,6 @@ if __name__ == "__main__":
     parser.add_argument("--latent_dim", type=int, default=2)
     parser.add_argument("--detach_latent", action="store_true")
     parser.add_argument("--lr", type=float, default=1e-3)
+    parser.add_argument("--K", type=int, default=1, help="Action chunk size")
     args = parser.parse_args()
     main(args)
