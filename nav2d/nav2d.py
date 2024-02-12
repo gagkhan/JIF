9261e21a8f836c325df535c10b0bb45371aae46c
Gagan Khandate <gagank@cs.columbia.edu>
2024-02-12 15:23:16 +0000

    VAE, New dataset return format

--- nav2d/nav2d.py
+++ nav2d/nav2d.py
@@ -1,31 +1,32 @@
+import time
 
-import numpy as np
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 
 class Map2D:
 
     def __init__(self) -> None:
-        
+
         self.arena_radius = 1.5
         self.arena_center = (0, 0)
 
         self.obstacle_radius = 0.2
         self.obstacle_center_radius = 0.7
-        
+
         self.obstacle_angles = np.array([45, 135, -45, -135])
-        
+
     def _collision_check(self, p):
         # Check if the new node is valid
         if np.linalg.norm(p) > self.arena_radius:
             return False
         for angle in self.obstacle_angles:
-            obstacle_center = self.obstacle_center_radius * np.array([ np.cos(np.deg2rad(angle)), np.sin(np.deg2rad(angle))])
+            obstacle_center = self.obstacle_center_radius * np.array(
+                [np.cos(np.deg2rad(angle)), np.sin(np.deg2rad(angle))]
+            )
             if np.linalg.norm(p - obstacle_center) < self.obstacle_radius:
                 return False
         return True
-
 
 
 class RRTExpert:
@@ -38,7 +39,6 @@ class RRTExpert:
 
         self.goal_reached = False
         self.edge_length = 0.1
-
 
     def plan(self, start, goal):
         self.start = start
@@ -56,9 +56,8 @@ class RRTExpert:
 
         self._get_path()
 
-
     def _extend(self):
-        
+
         # Sample a random point
         p_rand = self._sample_random_point()
 
@@ -74,37 +73,32 @@ class RRTExpert:
             self.edges = np.vstack([self.edges, p_new - p_near])
             self.parents.append(np.argmin(np.linalg.norm(self.nodes - p_near, axis=1)))
 
-
     def _sample_random_point(self):
         # Sample a random point
         x = np.random.uniform(-self.map.arena_radius, self.map.arena_radius)
         y = np.random.uniform(-self.map.arena_radius, self.map.arena_radius)
         return np.array([x, y])
-    
 
     def _find_nearest_node(self, p_rand):
         # Find the nearest node
         distances = np.linalg.norm(self.nodes - p_rand, axis=1)
         p_near = self.nodes[np.argmin(distances)]
         return p_near
-    
+
     def _find_new_node(self, p_near, p_rand):
         # Find the new node
         edge = self.edge_length * (p_rand - p_near) / np.linalg.norm(p_rand - p_near)
         p_new = p_near + edge
         return p_new
-    
 
     def _is_valid_node(self, p_new):
         return self.map._collision_check(p_new)
-    
 
     def _check_goal_reached(self):
         # Check if the goal is reached
         if np.linalg.norm(self.nodes[-1] - self.goal) < self.edge_length:
             self.goal_reached = True
 
-    
     def _get_path(self):
         # Get the path
         path = []
@@ -116,7 +110,8 @@ class RRTExpert:
         path.reverse()
         path = np.array(path)
         path_edges = path[1:] - path[:-1]
-        return path, path_edges    
+        return path, path_edges
+
 
 def single_demo_test():
 
@@ -141,28 +136,29 @@ def single_demo_test():
     print(path_edges)
 
     # Plot path
-    plt.plot(path[:, 0], path[:, 1], color='yellow')
+    plt.plot(path[:, 0], path[:, 1], color="yellow")
     # plt.fill_between(path[:, 0], path[:, 1], color='yellow')
 
     # Plot arena
-    arena = plt.Circle(map.arena_center, map.arena_radius, color='green')
+    arena = plt.Circle(map.arena_center, map.arena_radius, color="green")
     plt.gcf().gca().add_artist(arena)
 
     # Plot obstacles
     for angle in map.obstacle_angles:
-        obstacle_center = map.obstacle_center_radius * np.array([ np.cos(np.deg2rad(angle)), np.sin(np.deg2rad(angle))])
-        obstacle = plt.Circle(obstacle_center, map.obstacle_radius, color='black')
+        obstacle_center = map.obstacle_center_radius * np.array(
+            [np.cos(np.deg2rad(angle)), np.sin(np.deg2rad(angle))]
+        )
+        obstacle = plt.Circle(obstacle_center, map.obstacle_radius, color="black")
         plt.gcf().gca().add_artist(obstacle)
 
     # Plot start and goal
-    plt.plot(start[0], start[1], 'o', color='red')
-    plt.plot(goal[0], goal[1], 'o', color='blue')
-    
+    plt.plot(start[0], start[1], "o", color="red")
+    plt.plot(goal[0], goal[1], "o", color="blue")
 
     # Set plot limits and aspect ratio
     plt.xlim(-map.arena_radius, map.arena_radius)
     plt.ylim(-map.arena_radius, map.arena_radius)
-    plt.gca().set_aspect('equal', adjustable='box')
+    plt.gca().set_aspect("equal", adjustable="box")
 
     # Display the plot
     plt.show()
@@ -175,16 +171,103 @@ def collect_demo_dataset(num_demo=1000):
     for i in range(num_demo):
         angles = np.random.uniform(-180, 180, size=2)
         radius = np.random.uniform(0, map.arena_radius, size=2)
-        start = radius[0] * np.array([ np.cos(np.deg2rad(angles[0])), np.sin(np.deg2rad(angles[0]))])
-        goal = radius[1] * np.array([ np.cos(np.deg2rad(angles[1])), np.sin(np.deg2rad(angles[1]))])
+        start = radius[0] * np.array([np.cos(np.deg2rad(angles[0])), np.sin(np.deg2rad(angles[0]))])
+        goal = radius[1] * np.array([np.cos(np.deg2rad(angles[1])), np.sin(np.deg2rad(angles[1]))])
         if expert._is_valid_node(start) and expert._is_valid_node(goal):
             expert.plan(start, goal)
             path, path_edges = expert._get_path()
             print(path)
             print(path_edges)
-            print('----------------------')
+            print("----------------------")
 
-if __name__ == '__main__':
+
+class Robot:
+    """2D robot simulator"""
+
+    def __init__(self, map: Map2D) -> None:
+        self.map = map
+
+        # Initial position and goal position of the robot
+        # Call reset() to initialize
+        self.pos = np.array([0, 0])
+        self.goal = np.array([0, 0])
+
+        # Rendering
+        self.fig = None
+        self.t = 0
+        self.reset_called_once = False
+
+    def step(self, action):
+        # Call reset() first because the robot needs to be initialized
+        if self.reset_called_once is False:
+            raise Exception("Call reset() first")
+
+        assert action.shape == self.pos.shape, "action shape does not match"
+        new_pos = self.pos + action
+        if self.map._collision_check(new_pos):
+            self.pos = new_pos
+
+        self.t += 1
+        self.render()
+        if np.linalg.norm(self.pos - self.goal) < 0.1 or self.t > 50:
+            self.reset()
+        # obs = np.concatenate([self.pos, self.goal])
+        return self.pos.copy()
+
+    def render(self):
+        # Render the robot and goal position
+        if self.fig is None:
+            # Create figure and axis for rendering the map.
+            # Executed only once and the data is updated in subsequent calls
+            self.fig = plt.figure()
+            self.ax = self.fig.add_subplot(111)
+
+            # Plot arena
+            arena = plt.Circle(self.map.arena_center, self.map.arena_radius, color="green")
+            self.ax.add_artist(arena)
+
+            # Plot obstacles
+            for angle in self.map.obstacle_angles:
+                obstacle_center = self.map.obstacle_center_radius * np.array(
+                    [np.cos(np.deg2rad(angle)), np.sin(np.deg2rad(angle))]
+                )
+                obstacle = plt.Circle(obstacle_center, self.map.obstacle_radius, color="black")
+                self.ax.add_artist(obstacle)
+
+            self.vis_robot = self.ax.plot(self.pos[0], self.pos[1], "o", color="red")[0]
+            self.vis_goal = self.ax.plot(self.goal[0], self.goal[1], "o", color="blue")[0]
+
+            self.ax.set_xlim([-self.map.arena_radius, self.map.arena_radius])
+            self.ax.set_ylim([-self.map.arena_radius, self.map.arena_radius])
+            self.ax.set_aspect("equal", adjustable="box")
+            plt.show(block=False)
+
+        self.vis_robot.set_data(self.pos)
+        self.vis_goal.set_data(self.goal)
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
+        time.sleep(0.05)
+
+    def reset(self):
+        self.reset_called_once = True
+        while True:
+            # Sample start and goal positions, until they are both valid
+            angles = np.random.uniform(-180, 180, size=2)
+            radius = np.random.uniform(0, self.map.arena_radius, size=2)
+            start = radius[0] * np.array(
+                [np.cos(np.deg2rad(angles[0])), np.sin(np.deg2rad(angles[0]))]
+            )
+            goal = radius[1] * np.array(
+                [np.cos(np.deg2rad(angles[1])), np.sin(np.deg2rad(angles[1]))]
+            )
+            if self.map._collision_check(start) and self.map._collision_check(goal):
+                self.pos = start
+                self.goal = goal
+                break
+        self.t = 0
+        return self.pos.copy(), goal.copy()
+
+
+if __name__ == "__main__":
 
     single_demo_test()
-
--- nav2d/test.py
+++ nav2d/test.py
@@ -1,93 +1,17 @@
-from nav2d import Map2D
-from models import OIL
+import argparse
+
 import numpy as np
 import torch
-import time
-import matplotlib.pyplot as plt
-import argparse
-import time
-
-
-class Robot:
-    def __init__(self, map: Map2D) -> None:
-        self.map = map
-        self.pos = np.array([0, 0])
-        self.goal = np.array([0, 0])
-
-        # Rendering
-        self.fig = None
-        self.t = 0
-
-    def step(self, action):
-        assert action.shape == self.pos.shape, "action shape does not match"
-        new_pos = self.pos + action
-        if self.map._collision_check(new_pos):
-            self.pos = new_pos
-
-        self.t += 1
-        self.render()
-        if np.linalg.norm(self.pos - self.goal) < 0.1 or self.t > 50:
-            self.reset()
-        obs = np.concatenate([self.pos, self.goal])
-
-        return obs
-
-    def render(self):
-        if self.fig is None:
-            self.fig = plt.figure()
-            self.ax = self.fig.add_subplot(111)
-
-            # Plot arena
-            arena = plt.Circle(self.map.arena_center, self.map.arena_radius, color="green")
-            self.ax.add_artist(arena)
-
-            # Plot obstacles
-            for angle in self.map.obstacle_angles:
-                obstacle_center = self.map.obstacle_center_radius * np.array(
-                    [np.cos(np.deg2rad(angle)), np.sin(np.deg2rad(angle))]
-                )
-                obstacle = plt.Circle(obstacle_center, self.map.obstacle_radius, color="black")
-                self.ax.add_artist(obstacle)
-
-            self.vis_robot = self.ax.plot(self.pos[0], self.pos[1], "o", color="red")[0]
-            self.vis_goal = self.ax.plot(self.goal[0], self.goal[1], "o", color="blue")[0]
-
-            self.ax.set_xlim([-self.map.arena_radius, self.map.arena_radius])
-            self.ax.set_ylim([-self.map.arena_radius, self.map.arena_radius])
-            self.ax.set_aspect("equal", adjustable="box")
-            plt.show(block=False)
-
-        self.vis_robot.set_data(self.pos)
-        self.vis_goal.set_data(self.goal)
-        self.fig.canvas.draw()
-        self.fig.canvas.flush_events()
-        time.sleep(0.05)
-
-    def reset(self):
-        while True:
-            # Sample start and goal positions, until they are both valid
-            angles = np.random.uniform(-180, 180, size=2)
-            radius = np.random.uniform(0, self.map.arena_radius, size=2)
-            start = radius[0] * np.array(
-                [np.cos(np.deg2rad(angles[0])), np.sin(np.deg2rad(angles[0]))]
-            )
-            goal = radius[1] * np.array(
-                [np.cos(np.deg2rad(angles[1])), np.sin(np.deg2rad(angles[1]))]
-            )
-            if self.map._collision_check(start) and self.map._collision_check(goal):
-                self.pos = start
-                self.goal = goal
-                break
-        obs = np.concatenate([self.pos, self.goal])
-        self.t = 0
-        return obs
+from models import OIL
+from nav2d import Map2D, Robot
 
 
 def test(args):
     print("Creating model...")
 
     model = OIL(
-        obs_dim=4,
+        obs_dim=2,
+        goal_dim=2,
         act_dim=2,
         hidden_dim=args.hidden_dim,
         num_hidden=args.num_hidden,
@@ -102,15 +26,20 @@ def test(args):
 
     print("Model loaded.")
 
+    # Create map and robot
+    print("Creating map and robot...")
     map = Map2D()
     # map.obstacle_radius = 0.125  # shrink obstacles to simulate padding
     robot = Robot(map)
 
-    obs = robot.reset()
+    
+    print("Testing model...")
+    obs, goal = robot.reset()
     action_buffer = np.zeros((10, 2))
-
     for i in range(1000):
-        obs_tensor = torch.tensor(obs, dtype=torch.float32, device="cpu")
+        # obs_tensor = torch.tensor(obs, dtype=torch.float32, device="cpu")
+        model_input = np.concatenate([obs, goal])
+        obs_tensor = torch.tensor(model_input, dtype=torch.float32, device="cpu")
         obs_tensor = obs_tensor.unsqueeze(0)
         next_obs_pred, action = model(obs_tensor)
         action_buffer = np.vstack([action_buffer[1:, :], np.zeros((1, 2))])
@@ -120,14 +49,18 @@ def test(args):
 
         print("action:", action_buffer[0])
         obs = robot.step(action_buffer[0])
+        print("pos:", next_obs_pred[0].detach().cpu().numpy() - obs)
         print("obs:", obs)
 
-        if np.linalg.norm(obs[:2] - obs[2:]) < 0.1:
+        if np.linalg.norm(obs[:2] - goal) < 0.1:
             print("Goal reached!")
+            # No need to call reset() because robot.step() will call it
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, default="oil.pt")
+    # TODO: Infer model hyperparameters from the model
     parser.add_argument("--hidden_dim", type=int, default=64)
     parser.add_argument("--num_hidden", type=int, default=2)
     parser.add_argument("--latent_dim", type=int, default=2)
