066a58f28239737372476baba62e7f3f659a48f1
Gagan Khandate <gagank@cs.columbia.edu>
2024-03-23 01:49:37 +0000

    Update README

--- scripts/cpt_nav2d.sh
+++ scripts/cpt_nav2d.sh
@@ -6,17 +6,35 @@ if [ -z "$1" ]
     echo "Output directory argument not provided"
 else
     OUTDIR=$RUNDIR/$1
-
+fi
+  
     python ../visual/main_cpt.py \
     --arch vit_tiny \
     --measure l2 \
     --data_path /home/gagan/Home/VideoIL/data/nav2d/visual_v1 \
-    --local_crops_number 0 \
-    --global_crops_scale 0.95 1.0 \
-    --norm_last_layer False \
-    --latent_action_dim 2 \
     --output_dir $OUTDIR \
-    --beta 0.1 \
-    --skip_frames 1
-
+    --alpha 0 \
+    --global_crops_scale 0.99 1.0 \
+    --local_crops_scale 0.99 1.0 \
+    --epochs 2000
+   
+    # python ../visual/main_cpt.py \
+    # --arch vit_tiny \
+    # --measure l2 \
+    # --data_path /home/gagan/Home/VideoIL/data/nav2d/visual_v1 \
+    # --output_dir $OUTDIR \
+    # --alpha 10 \
+    # --global_crops_scale 0.99 1.0 \
+    # --local_crops_number 0 \
+    # --norm_last_layer False \
+    # --out_dim 2 \
+    # --latent_action_dim 2 \
+    # --warmup_epochs 1 \
+    # # --local_crops_scale 0.99 1.0 \
+    # --latent_action_dim 128 \
+    # --beta 0.001 \
+    # --skip_frames 1 \
+    # --epochs 2000\
+    # --lr 0.01 
+    #  --local_crops_number 0 \
 fi
--- scripts/cpt_ours.sh
+++ scripts/cpt_ours.sh
@@ -9,11 +9,21 @@ else
 fi
 
 
+# python ../visual/main_cpt.py \
+#     --arch vit_tiny \
+#     --measure cross_entropy \
+#     --data_path /home/gagan/Home/VideoIL/data/ours/ours_v2_frames \
+#     --local_crops_scale 0.99 1.0 \
+#     --local_crops_scale 0.99 1.0 \
+#     --epochs 2000 \
+#     --output_dir $OUTDIR \
+
 python ../visual/main_cpt.py \
     --arch vit_tiny \
     --measure cross_entropy \
     --data_path /home/gagan/Home/VideoIL/data/ours/ours_v2_frames \
-    --local_crops_scale 0.4 1.0 \
-    --epochs 2000 \
-    --output_dir $OUTDIR
-    
+    --local_crops_scale 0.99 1.0 \
+    --local_crops_scale 0.99 1.0 \
+    --epochs 100 \
+    --output_dir $OUTDIR \
+    --lr 0.00075 \
