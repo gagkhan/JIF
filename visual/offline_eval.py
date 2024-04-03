94f99872e91febb4d6fbbde75cf48905c983c146
Gagan Khandate <gagank@cs.columbia.edu>
2024-04-03 18:35:42 +0000

    Add script to perform offline evaluation w.r.t robot demonstrations

---
+++ visual/offline_eval.py
@@ -0,0 +1,247 @@
+# This script for offline evaluation of the model
+# We will load the demonstration trajectories and the model and evaluate the model
+# on these trajectories.
+
+
+# how to load the demonstration data as trajectories
+# what is the input?
+# what is the output?
+
+# input is the path to the robot demonstration data
+
+# iterator over the trajetories (sequence of images, sequence of actions)
+
+# for each trajectory, we will:
+# 1. load the images and the actions
+# 2. run the model on the images
+# 3. compare the predicted actions with the actual actions
+# 4. report the error
+
+# What to implement?
+# A dataset class that loads the demonstration data
+
+import argparse
+import os
+import pathlib as pl
+from pathlib import Path
+
+import numpy as np
+import torch
+import visual.vision_transformer as vits
+from matplotlib.patches import Polygon
+from PIL import Image
+from torchvision.transforms import ToTensor
+from visual.ilpo import MLP, ActionDecoder, Dynamics, ILPOWrapper, Policy
+
+
+class SequenceDataset:
+    """
+    A dataset class for loading sequence data.
+
+    Args:
+        data_root (str): The root directory of the data.
+
+    Attributes:
+        data_root (str): The root directory of the data.
+        data (list): A list of directories containing the data.
+        transform (ToTensor): An instance of the ToTensor class for transforming the data.
+
+    Methods:
+        load_data(): Loads the data from the data root directory.
+        __len__(): Returns the length of the dataset.
+        __getitem__(idx): Returns the item at the given index.
+
+    """
+
+    def __init__(self, data_root):
+        self.data_root = pl.Path(data_root)
+        self.data = self.load_data()
+
+        print(f"Loaded {len(self.data)} demonstrations")
+        self.transform = ToTensor()
+
+    def load_data(self):
+        """
+        Loads the data from the data root directory.
+
+        Returns:
+            list: A list of directories containing the data.
+
+        """
+        data = []
+        for demo in os.listdir(self.data_root):
+            if os.path.isdir(os.path.join(self.data_root, demo)):
+                data.append(demo)
+        return data
+
+    def __len__(self):
+        """
+        Returns the length of the dataset.
+
+        Returns:
+            int: The length of the dataset.
+
+        """
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        """
+        Returns the item at the given index.
+
+        Args:
+            idx (int): The index of the item to retrieve.
+
+        Returns:
+            tuple: A tuple containing the observations and actions.
+
+        """
+        # open the demo folder
+        demo = self.data[idx]
+        demo_path = os.path.join(self.data_root, demo)
+        obs = []
+        image_paths = []
+        for frame in sorted(os.listdir(demo_path)):
+            if frame.endswith(".jpg") or frame.endswith(".png"):
+                image_paths.append(frame)
+        for image_path in image_paths:
+            image = self.transform(Image.open(os.path.join(demo_path, image_path)))
+            obs.append(image)
+        actions = np.load(os.path.join(demo_path, "actions.npy"))
+
+        obs = torch.stack(obs)
+        actions = torch.tensor(actions, dtype=torch.float32)
+
+        return obs, actions
+
+
+def load_model(args, device):
+    """Load model from checkpoint."""
+
+    assert os.path.isfile(args.pretrained_weights), "No pretrained weights found or file invalid"
+
+    state_dict = torch.load(args.pretrained_weights, map_location="cpu")
+
+    model_args = state_dict["args"]
+
+    # build vit
+    transformer = vits.__dict__[args.arch](patch_size=args.patch_size, num_classes=0)
+
+    embed_dim = transformer.embed_dim
+    head = vits.DINOHead(embed_dim, model_args.out_dim, model_args.use_bn_in_head)
+
+    # build ILPO student
+    model = ILPOWrapper(
+        transformer,
+        head,
+        embed_dim=embed_dim,
+        latent_action_dim=model_args.latent_action_dim,
+        policy_units=model_args.policy_units,
+        dynamics_units=model_args.dynamics_units,
+    )
+
+    action_decoder = ActionDecoder(
+        model_args.latent_action_dim,
+        units=model_args.action_decoder_units,
+        action_shape=(model_args.skip_frames + 1, 3),
+    )
+
+    # load weights
+    # remove `module.` prefix
+    state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
+    #     # remove `backbone.` prefix induced by multicrop wrapper
+    state_dict = {k.replace("backbone.", ""): v for k, v in state_dict.items()}
+    model.load_state_dict(state_dict["student"], strict=False)
+
+    action_decoder.load_state_dict(state_dict["action_decoder"], strict=False)
+
+    for p in model.parameters():
+        p.requires_grad = False
+    for p in action_decoder.parameters():
+        p.requires_grad = False
+    model.eval()
+    action_decoder.eval()
+    model.to(device)
+    action_decoder.to(device)
+
+    return model, action_decoder
+
+
+def get_arg_parser():
+    """Get argument parser."""
+
+    parser = argparse.ArgumentParser("Evaluate model for Visual Navigation.")
+
+    parser.add_argument(
+        "--data_root",
+        default="/home/gagan/Home/VideoIL/data/ours/MoveT",
+        type=str,
+        help="The root directory of the data.",
+    )
+
+    parser.add_argument(
+        "--arch",
+        default="vit_tiny",
+        type=str,
+        choices=["vit_tiny", "vit_small", "vit_base"],
+        help="Architecture (support only ViT atm).",
+    )
+
+    parser.add_argument("--patch_size", default=16, type=int, help="Patch resolution of the model.")
+    parser.add_argument(
+        "--pretrained_weights", default="", type=str, help="Path to pretrained weights to load."
+    )
+    parser.add_argument(
+        "--checkpoint_key",
+        default="teacher",
+        type=str,
+        help='Key to use in the checkpoint (example: "teacher")',
+    )
+    parser.add_argument(
+        "--image_size", default=(480, 480), type=int, nargs="+", help="Resize image."
+    )
+    parser.add_argument("--output_dir", default=".", help="Path where to save visualizations.")
+    parser.add_argument(
+        "--threshold",
+        type=float,
+        default=None,
+        help="""We visualize masks
+        obtained by thresholding the self-attention maps to keep xx% of the mass.""",
+    )
+    return parser
+
+
+def run_offline_evaluation(
+    data_root,
+    model,
+    action_decoder,
+    device,
+):
+
+    dataset = SequenceDataset(data_root)
+
+    errors = []
+    for ep in range(len(dataset)):
+        obs, actions = dataset[ep]
+        obs = obs.to(device)
+        actions = actions.to(device)
+
+        print(f"Episode: {ep}")
+
+        # get latent actions from the ILPO model
+        for t in range(len(obs)):
+            _, zt, zmu, zsigma = model(obs[t].unsqueeze(0), obs[-1].unsqueeze(0))
+            # get the action from the action decoder
+            action = action_decoder(zt)
+            error = torch.norm(action[0] - actions[t]).cpu().numpy()
+            print(error)
+
+
+if __name__ == "__main__":
+
+    argparser = get_arg_parser()
+    args = argparser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model, action_decoder = load_model(args, device)
+
+    run_offline_evaluation(args.data_root, model, action_decoder, device)
