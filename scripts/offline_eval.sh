c96724420e775216321b695546afaad8ba389da0
gagkhan <gagank@cs.columbia.edu>
2024-04-18 11:40:42 -0400

    Update offline evaluation script

--- scripts/offline_eval.sh
+++ scripts/offline_eval.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-python offline_eval.py \
-    --pretrained_weights /home/gagan/Home/VideoIL/runs/ours/march26/test/checkpoint.pth \
+python ../visual/offline_eval.py \
+    --checkpoint_key student \
+    --arch vit_small \
+    --pretrained_weights /home/gagan/Home/VideoIL/runs/tabletop/apr13_finetune/ladim-64/checkpoint.pth \
     --data_root /home/gagan/Home/VideoIL/data/eval/tabletop_robot/moveT
