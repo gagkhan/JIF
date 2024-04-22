b38dd3e0021aedeffd5fc9421374d1ad8ddb38a8
Gagan Khandate <gagank@cs.columbia.edu>
2024-04-22 22:24:25 +0000

    Add new experiments, Add act_dec params to optimizer and grad clipping

---
+++ scripts/april22_cpt_tabletop_finetune.sh
@@ -0,0 +1,24 @@
+RUNDIR="../../runs/ours/tabletop"
+
+if [ -z "$1" ]
+    then
+    echo "Output directory argument not provided"
+else
+    OUTDIR=$RUNDIR/$1
+fi
+
+for ladim in 32 16 64 8 ; do
+    python ../visual/main_cpt.py \
+        --arch vit_small \
+        --measure cross_entropy \
+        --data_path /home/gagan/Home/VideoIL/data/ours/ours_tabletop/ours_tabletop_mix_v2 \
+        --local_crops_scale 0.99 1.0 \
+        --global_crops_scale 0.99 1.0 \
+        --local_crops_number 0 \
+        --epochs 100 \
+        --output_dir $OUTDIR/ladim-$ladim \
+        --lr 0.001 \
+        --latent_action_dim $ladim \
+        --alpha 10 \
+        --pretrained_weights /home/gagan/Home/VideoIL/dino_deitsmall16_pretrain_full_checkpoint.pth
+done
