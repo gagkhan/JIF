d9fc98d28af314dd4519eb69ff4e76abc8aed8d1
Gagan Khandate <gagank@cs.columbia.edu>
2024-06-08 03:31:46 +0000

    Add script for skip_frames ablation without goal conditioning

--- scripts/jun_7_skipframes_ablation.sh
+++ scripts/jun_7_skipframes_ablation.sh
@@ -1,4 +1,5 @@
-RUNDIR="/proj/vondrick4/gagan/VideoIL/runs"
+# RUNDIR="/proj/vondrick4/gagan/VideoIL/runs"
+RUNDIR="../../runs"
 
 if [ -z "$1" ]
     then
@@ -7,47 +8,37 @@ else
     OUTDIR=$RUNDIR/$1
 fi
 
-# expt1
-# export CUDA_VISIBLE_DEVICES=2
-# latent_action_cond=True
-# i=0
-# ladim=16
 
-# export CUDA_VISIBLE_DEVICES=3
-# latent_action_cond=True
-# i=1
-# ladim=16
+export CUDA_VISIBLE_DEVICES=1
+latent_action_cond=False
+i=0
+ladim=16
+skip_frames=20
 
-# export CUDA_VISIBLE_DEVICES=4
-# latent_action_cond=True
-# i=2
-# ladim=16
+export CUDA_VISIBLE_DEVICES=1
+latent_action_cond=False
+i=1
+ladim=16
+skip_frames=20
 
-# export CUDA_VISIBLE_DEVICES=5
-# latent_action_cond=False
-# i=0
-# ladim=16
-
-# export CUDA_VISIBLE_DEVICES=6
-# latent_action_cond=False
-# i=1
-# ladim=16
-
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=1
 latent_action_cond=False
 i=2
 ladim=16
+skip_frames=20
+
 python ../visual/main_cpt.py \
     --arch vit_tiny \
     --measure cross_entropy \
-    --data_path /proj/vondrick4/gagan/VideoIL/data/ours/ours_tabletop/ours_tabletop_mix_v2 \
+    --data_path /home/gagan/Home/VideoIL/data/ours/ours_tabletop/ours_tabletop_mix_v2 \
     --local_crops_scale 0.99 1.0 \
     --global_crops_scale 0.99 1.0 \
     --epochs 100 \
-    --output_dir $OUTDIR/ladim-$ladim/trial-$i \
+    --output_dir $OUTDIR/ladim-$16/trial-$i \
     --lr 0.001 \
     --latent_action_dim $ladim \
     --alpha 0 \
-    --latent_action_cond $latent_action_cond
+    --latent_action_cond $latent_action_cond \
+    --goal_cond False
 
 
