ba17cedfd78755be9ca5f0b4a10ea449c674a9c7
Gagan <gagank@cs.columbia.edu>
2024-06-11 16:45:35 -0400

    Update skipframes ablations script to run on CV serser

--- scripts/jun_7_skipframes_ablation.sh
+++ scripts/jun_7_skipframes_ablation.sh
@@ -1,5 +1,5 @@
-# RUNDIR="/proj/vondrick4/gagan/VideoIL/runs"
-RUNDIR="../../runs"
+RUNDIR="/proj/vondrick4/gagan/VideoIL/runs"
+# RUNDIR="../../runs"
 
 if [ -z "$1" ]
     then
@@ -9,36 +9,55 @@ else
 fi
 
 
-export CUDA_VISIBLE_DEVICES=1
-latent_action_cond=False
-i=0
-ladim=16
-skip_frames=20
+# export CUDA_VISIBLE_DEVICES=2
+# latent_action_cond=False
+# i=0
+# ladim=16
+# skip_frames=20
 
-export CUDA_VISIBLE_DEVICES=1
-latent_action_cond=False
-i=1
-ladim=16
-skip_frames=20
+# export CUDA_VISIBLE_DEVICES=3
+# latent_action_cond=False
+# i=1
+# ladim=16
+# skip_frames=20
 
-export CUDA_VISIBLE_DEVICES=1
+# export CUDA_VISIBLE_DEVICES=4
+# latent_action_cond=False
+# i=2
+# ladim=16
+# skip_frames=20
+
+# export CUDA_VISIBLE_DEVICES=5
+# latent_action_cond=False
+# i=0
+# ladim=16
+# skip_frames=40
+
+# export CUDA_VISIBLE_DEVICES=6
+# latent_action_cond=False
+# i=1
+# ladim=16
+# skip_frames=40
+
+export CUDA_VISIBLE_DEVICES=7
 latent_action_cond=False
 i=2
 ladim=16
-skip_frames=20
+skip_frames=40
 
 python ../visual/main_cpt.py \
     --arch vit_tiny \
     --measure cross_entropy \
-    --data_path /home/gagan/Home/VideoIL/data/ours/ours_tabletop/ours_tabletop_mix_v2 \
+    --data_path /proj/vondrick4/gagan/VideoIL/data/ours/ours_tabletop/ours_tabletop_mix_v2 \
     --local_crops_scale 0.99 1.0 \
     --global_crops_scale 0.99 1.0 \
     --epochs 100 \
-    --output_dir $OUTDIR/ladim-$16/trial-$i \
+    --output_dir $OUTDIR/skipframes_$skip_frames/trial-$i \
     --lr 0.001 \
     --latent_action_dim $ladim \
     --alpha 0 \
     --latent_action_cond $latent_action_cond \
-    --goal_cond False
+    --goal_cond False \
+    --skip_frames $skip_frames
 
 
