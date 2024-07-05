4450ab6a99f7f60512e50999e58a73965ac1d9d7
Gagan Khandate <gagank@cs.columbia.edu>
2024-07-05 21:14:48 +0000

    Training script

---
+++ scripts/july5_cpt_vae.sh
@@ -0,0 +1,28 @@
+RUNDIR="$PROJDIR/runs"
+
+if [ -z "$1" ]
+    then
+    echo "Output directory argument not provided"
+else
+    OUTDIR=$RUNDIR/$1
+fi
+
+for beta1 in 0.0001 0.0005; do
+    for beta2 in 0.0001; do
+        for i in 0 1 2; do
+            python $PROJDIR/CPT/cpt/main_cpt_vqvae.py \
+                --encoder_arch vit_tiny \
+                --data_path /home/gagan/Home/VideoIL/data/ours/ours_tabletop/ours_tabletop_mix_v2 \
+                --output_dir $OUTDIR-beta1_$beta1-beta2_$beta2 \
+                --beta1 $beta1 \
+                --beta2 $beta2 \
+                --alpha 0 \
+                --core lapo \
+                --epochs 200 \
+                --skip_frames 20 
+        done
+    done
+done
+    
+
+    
