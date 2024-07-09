7b02ed9d244760d0c8fc744cdfb67054b571bef5
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-07-09 13:32:01 -0400

    better format

--- bet/utils.py
+++ bet/utils.py
@@ -1,7 +1,7 @@
 import bet.model as bets
+from torch import nn
 
-
-def build_bet(args, input_dim):
+def build_bet(args, input_dim) -> nn.Module:
 
     if args.bet_arch in bets.__dict__.keys():
         return bets.__dict__[args.bet_arch](input_dim, args.context_len, args.num_actions, args.causal)
