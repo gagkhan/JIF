61bca06a15fe87cfa220eac4598e04eb9f985285
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-07-09 20:47:20 -0400

    debug

--- bet/vq_actions.py
+++ bet/vq_actions.py
@@ -1,6 +1,7 @@
 import argparse, datetime, time, json, os
 from pathlib import Path
 
+import wandb
 import torch
 from torch import nn, Tensor
 from torchvision import transforms
@@ -149,6 +150,7 @@ def train_vq(args):
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print("Training time {}".format(total_time_str))
+    wandb.finish()
 
 
 def train_one_epoch(
