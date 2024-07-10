ba9589acacc30b6a3dc9145ddb65160fe827c699
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-07-10 18:25:08 -0400

    hyperparam tuning

--- bet/vq_actions.py
+++ bet/vq_actions.py
@@ -93,8 +93,8 @@ def train_vq(args):
     action_quantizer = ActionQuantizer(
         action_dim=3, 
         action_chunk_size=args.action_chunk_len,
-        encoder_units=[256,128,32],
-        decoder_units=[32,128,256],
+        encoder_units=[128,128,128,128],
+        decoder_units=[128,128,128,128],
         embedding_dim=8,
         num_embeddings=32,
     )
