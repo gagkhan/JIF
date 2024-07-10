2224a95e100aa6164eac18b0f565b8177c8f838c
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-07-10 10:17:39 -0400

    debug

--- bet/vq_actions.py
+++ bet/vq_actions.py
@@ -181,8 +181,8 @@ def save_actions_plot_one_epoch(action_pairs, save_path, num_pairs=1) -> Axes:
         actions, actions_recon = action_pairs[p]
         assert(actions.shape == actions_recon.shape)
         # Get points to plot
-        actions_cumu       = torch.cumsum(actions,       dim=0).numpy().T # (3, action_chunk_size)
-        actions_recon_cumu = torch.cumsum(actions_recon, dim=0).numpy().T # (3, action_chunk_size)
+        actions_cumu       = torch.cumsum(actions,       dim=0).cpu().numpy().T # (3, action_chunk_size)
+        actions_recon_cumu = torch.cumsum(actions_recon, dim=0).cpu().numpy().T # (3, action_chunk_size)
         # Plot 
         ax = plt.figure().add_subplot(projection='3d')
         ax.plot(actions_cumu      [0], actions_cumu      [1], actions_cumu      [2], \
