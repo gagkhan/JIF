50d677265c246376b798f2c36327d497b1696124
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-07-11 13:25:10 -0400

    debug

--- bet/vq_actions.py
+++ bet/vq_actions.py
@@ -197,9 +197,9 @@ def save_actions_plot_one_epoch(action_pairs, file_path, num_pairs=1) -> Axes:
         actions_recon_cumu = torch.cumsum(actions_recon, dim=0).cpu().detach().numpy().T # (3, action_chunk_size)
         # Plot curves
         ax.plot(actions_cumu      [0], actions_cumu      [1], actions_cumu      [2], \
-                zdir='z', label=f'actions {p}')
+                zdir='z', label=f'actions {p:02}')
         ax.plot(actions_recon_cumu[0], actions_recon_cumu[1], actions_recon_cumu[2], \
-                zdir='z', label=f'actions_recon {p}')
+                zdir='z', label=f'actions_recon {p:02}')
     
     # Save figure
     ax.set_xlim([-0.055, 0.055])
