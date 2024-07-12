ce2be9ea24f9c5bbb3d1965c2e8713568c661fe5
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-07-11 21:48:52 -0400

    revert back to constant lr

--- bet/vq_actions.py
+++ bet/vq_actions.py
@@ -112,9 +112,8 @@ def train_vq(args):
     
     # ============ init schedulers ... ============
 
-    lr_schedule = utils.linear_scheduler(
+    lr_schedule = utils.constant_scheduler(
         args.lr,
-        args.min_lr,
         args.epochs,
         len(data_loader)
     )
