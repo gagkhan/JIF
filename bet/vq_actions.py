affba989c02e5fe9d3670925c9d8fe903e320777
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-07-13 19:39:13 -0400

    v7.9

--- bet/vq_actions.py
+++ bet/vq_actions.py
@@ -100,7 +100,7 @@ def train_vq(args):
         decoder_units=[8,8,16,16], 
         embedding_dim=256,
         num_embeddings=32,
-        cmt_weight=1e-8,
+        cmt_weight=1e-10,
     )
     action_quantizer = action_quantizer.cuda()
 
