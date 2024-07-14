81da58c29eec9de8a025de09a61a97adeb2bd426
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-07-14 15:29:28 -0400

    v9.4

--- bet/vq_actions.py
+++ bet/vq_actions.py
@@ -331,7 +331,7 @@ def train_one_epoch(
 
         # loss
         criterion = get_loss
-        loss, recon_loss, cmt_loss, actions_cumu, actions_recon_cumu = \
+        loss, recon_loss, end_loss, cmt_loss, actions_cumu, actions_recon_cumu = \
             criterion(actions_recon, actions, cmt_loss)
 
         # optimizer step
@@ -346,6 +346,7 @@ def train_one_epoch(
         # logging metrics
         metric_logger.update(action_loss=      loss.item())
         metric_logger.update( recon_loss=recon_loss.item())
+        metric_logger.update(   end_loss=  end_loss.item())
         metric_logger.update(   cmt_loss=  cmt_loss.item())
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
         metric_logger.update(wd=optimizer.param_groups[0]["weight_decay"])
@@ -372,13 +373,17 @@ def get_loss(actions_recon: Tensor, actions: Tensor, cmt_loss: Tensor = 0.0):
     criterion = nn.MSELoss()
     recon_loss: Tensor = criterion(actions_recon, actions)
 
+    # endpoint loss
+    criterion = nn.CosineSimilarity()
+    end_loss: Tensor = 1e-4 * (1-criterion(actions_recon_cumu[:,-1,:].squeeze(), actions_cumu[:,-1,:].squeeze()).mean()) / actions.shape[1]
+
     # commitment loss
     cmt_loss = cmt_loss.squeeze()
 
     # loss
-    loss = recon_loss + cmt_loss 
+    loss = recon_loss + end_loss + cmt_loss 
 
-    return loss, recon_loss, cmt_loss, actions_cumu, actions_recon_cumu
+    return loss, recon_loss, end_loss, cmt_loss, actions_cumu, actions_recon_cumu
 
 
 if __name__ == "__main__":
