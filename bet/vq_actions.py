9f8d5931b34a5b3c4f6614d384a62731b5e9aea9
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-07-14 19:13:52 -0400

    14v0.4

--- bet/vq_actions.py
+++ bet/vq_actions.py
@@ -376,7 +376,7 @@ def get_loss(actions_recon: Tensor, actions: Tensor, cmt_loss: Tensor = 0.0):
 
     # endpoint loss
     criterion = nn.CosineSimilarity()
-    end_loss: Tensor = 1e-4 * (1-criterion(actions_recon_cumu[:,-1,:].squeeze(), actions_cumu[:,-1,:].squeeze()).mean()) / actions.shape[1]
+    end_loss: Tensor = 5e-4 * (1-criterion(actions_recon_cumu[:,-1,:].squeeze(), actions_cumu[:,-1,:].squeeze()).mean()) / actions.shape[1]
 
     # commitment loss
     cmt_loss = cmt_loss.squeeze()
