a160798e2a02c877d858a3edd5ce36f21a33762c
Gagan <gagank@cs.columbia.edu>
2024-07-15 09:10:19 -0400

    Remove action labelling from umap visualization to be compatible with bridge data

--- examples/dist_cpt_vqvae.sh
+++ examples/dist_cpt_vqvae.sh
@@ -9,6 +9,7 @@ fi
 
 
 # --data_path $PROJDIR/data/ours/ours_tabletop/ours_tabletop_mix_v2 \
+# --data_path $PROJDIR/data/ssv2/20bn-something-something-v2-frames-tiny \
 rm -rf $OUTDIR
 
 NUM_TRAINERS=8
@@ -21,7 +22,7 @@ torchrun \
     --nproc-per-node=$NUM_TRAINERS \
     $PROJDIR/CPT/cpt/main_cpt_vqvae.py \
     --encoder_arch vit_tiny \
-    --data_path $PROJDIR/data/ssv2/20bn-something-something-v2-frames-tiny \
+    --data_path $PROJDIR/data/bridge/raw/ \
     --output_dir $OUTDIR \
     --beta1 0.0001 \
     --beta2 0.0001 \
