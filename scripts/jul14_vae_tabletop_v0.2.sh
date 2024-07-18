64dae676b7bf48bced904dc3f0fd88d426a9ab7d
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-07-18 18:04:53 +0000

    added scripts for smaller codebook size action quantizer

---
+++ scripts/jul14_vae_tabletop_v0.2.sh
@@ -0,0 +1,20 @@
+RUNDIR="/ssd01/gagan/cpt_checkpoints"
+
+if [ -z "$1" ]
+    then
+    echo "Output directory argument not provided"
+else
+    OUTDIR=$RUNDIR/$1
+fi
+
+python ../bet/vq_actions.py \
+    --data_path /ssd01/gagan/cpt_data/ours/jun19_bc_tabletop_rgb \
+    --batch_size_per_gpu 16 \
+    --epochs 48 \
+    --output_dir $OUTDIR \
+    --lr 0.0001 \
+    --action_quantizer_encoder_units 16 16 \
+    --action_quantizer_decoder_units 16 16 \
+    --action_quantizer_embedding_dim 16    \
+    --action_quantizer_codebook_size 8     \
+    --action_quantizer_decay         0.9   \
