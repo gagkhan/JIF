4b230a430d4721ba1b35562fcf514c9bb02a9f25
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-07-22 20:52:40 +0000

    cleanup

--- bet/main_bet.py
+++ bet/main_bet.py
@@ -81,10 +81,7 @@ def train_bc(args):
         )["action_quantizer"])
     
     # Freeze weights and move to GPU
-    action_quantizer.eval()
-    for p in action_quantizer.parameters():
-        p.requires_grad = False
-    action_quantizer.freeze_vq_layer = True
+    action_quantizer.freeze()
     action_quantizer.cuda()
 
     # ============ building visual encoder network ... ============
