6ba3d5df02c6031b0c62673ede01385bca399019
Gagan Khandate <gagank@cs.columbia.edu>
2024-07-25 19:00:32 +0000

    validate() to run validation loop and logging

--- cpt/main_cpt_vqvae.py
+++ cpt/main_cpt_vqvae.py
@@ -197,7 +197,7 @@ def get_args_parser():
     parser.add_argument(
         "--latent_action_dim",
         type=int,
-        default=16,
+        default=3,
         help="""Dimensionality of the latent action i.e. output of the latent policy network""",
     )
 
@@ -305,16 +305,23 @@ def train_cpt(args):
     )
     # dataset = VisDemoDataset(data_root=args.data_path, transform=transform, skip_frames=args.skip_frames)
     dataset, val_dataset = load_dataset(args, wrapper_cls="VisDemoDataset", transform=transform)
-    sampler = torch.utils.data.DistributedSampler(dataset, shuffle=True)
     data_loader = torch.utils.data.DataLoader(
         dataset,
-        sampler=sampler,
+        sampler=torch.utils.data.DistributedSampler(dataset, shuffle=True),
         batch_size=args.batch_size_per_gpu,
         num_workers=args.num_workers,
         pin_memory=True,
         drop_last=True,
     )
-    print(f"Data loaded: there are {len(dataset)} images.")
+    val_data_loader = torch.utils.data.DataLoader(
+        val_dataset,
+        sampler=torch.utils.data.DistributedSampler(val_dataset, shuffle=True),
+        batch_size=args.batch_size_per_gpu,
+        num_workers=args.num_workers,
+        pin_memory=True,
+        drop_last=True,
+    )
+    # print(f"Data loaded: there are {len(dataset)} images.")
 
     # ============ building networks ... ============
 
@@ -398,6 +405,7 @@ def train_cpt(args):
         data_loader.sampler.set_epoch(epoch)
 
         # ============ training one epoch of CPT ... ============
+
         train_stats = train_one_epoch(
             encoder,
             decoder,
@@ -411,6 +419,18 @@ def train_cpt(args):
             fp16_scaler,
             args,
         )
+
+        val_stats = validate(
+            encoder,
+            decoder,
+            action_decoder,
+            recon_loss,
+            val_data_loader,
+            fp16_scaler,
+            args,
+        )
+
+        epoch_stats = {**train_stats, **val_stats}
 
         # ============ writing logs ... ============
         save_dict = {
@@ -427,11 +447,11 @@ def train_cpt(args):
         utils.save_on_master(save_dict, os.path.join(args.output_dir, "checkpoint.pth"))
         if args.saveckp_freq and epoch % args.saveckp_freq == 0:
             utils.save_on_master(save_dict, os.path.join(args.output_dir, f"checkpoint{epoch:04}.pth"))
-        log_stats = {**{f"train_{k}": v for k, v in train_stats.items()}, "epoch": epoch}
+        log_stats = {**{f"{k}": v for k, v in epoch_stats.items()}, "epoch": epoch}
         if utils.is_main_process():
             with (Path(args.output_dir) / "log.txt").open("a") as f:
                 f.write(json.dumps(log_stats) + "\n")
-            utils.wandb_log(train_stats, epoch=epoch)
+            utils.wandb_log(epoch_stats, epoch=epoch)
 
             if epoch % 2 == 0:
                 cpt.utils.log_recons(encoder, decoder, data_loader, epoch, args)
@@ -455,6 +475,10 @@ def train_one_epoch(
     fp16_scaler,
     args,
 ):
+    # put the models in train mode
+    for m in [encoder, decoder, action_decoder, recon_loss]:
+        m.train()
+
     metric_logger = utils.MetricLogger(delimiter="  ")
     header = "Epoch: [{}/{}]".format(epoch, args.epochs)
     for it, batch in enumerate(metric_logger.log_every(data_loader, 10, header)):
@@ -470,12 +494,7 @@ def train_one_epoch(
 
         o_curr = o_curr.cuda(non_blocking=True)
         o_next = o_next.cuda(non_blocking=True)
-
-        if args.core == "ilpo":
-            o_goal = o_goal.cuda(non_blocking=True)
-        else:
-            o_goal = None
-
+        o_goal = o_goal.cuda(non_blocking=True) if args.core == "ilpo" else None
         actions = actions.cuda(non_blocking=True)
         amask = amask.cuda(non_blocking=True)
 
@@ -494,7 +513,9 @@ def train_one_epoch(
             aloss = action_loss(actions_pred, actions, amask)
             z_reg_loss = torch.mean(z_reg_loss)
 
-            loss = rloss + args.alpha * aloss + args.beta1 * z_reg_loss + args.beta2 * x_reg_loss
+            loss = rloss + args.beta1 * z_reg_loss + args.beta2 * x_reg_loss
+            if args.alpha != 0:
+                loss += args.alpha * aloss
 
         if not math.isfinite(loss.item()):
             print("Loss is {}, stopping training".format(loss.item()), force=True)
@@ -522,11 +543,11 @@ def train_one_epoch(
 
         # logging
         torch.cuda.synchronize()
-        metric_logger.update(loss=loss.item())
-        metric_logger.update(recon_loss=rloss.item())
-        metric_logger.update(z_reg_loss=z_reg_loss.item())
-        metric_logger.update(x_reg_loss=x_reg_loss.item())
-        metric_logger.update(action_loss=aloss.item())
+        metric_logger.update(train_loss=loss.item())
+        metric_logger.update(train_recon_loss=rloss.item())
+        metric_logger.update(train_z_reg_loss=z_reg_loss.item())
+        metric_logger.update(train_x_reg_loss=x_reg_loss.item())
+        metric_logger.update(train_action_loss=aloss.item())
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
         metric_logger.update(wd=optimizer.param_groups[0]["weight_decay"])
 
@@ -534,9 +555,75 @@ def train_one_epoch(
     metric_logger.synchronize_between_processes()
     print("Averaged stats:", metric_logger)
 
-    train_stats = {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+    stats = {k: meter.global_avg for k, meter in metric_logger.meters.items()}
 
-    return train_stats
+    return stats
+
+
+def validate(
+    encoder,
+    decoder,
+    action_decoder,
+    recon_loss,
+    data_loader,
+    fp16_scaler,
+    args,
+):
+
+    # prepare for validation
+    for m in [encoder, decoder, action_decoder, recon_loss]:
+        m.eval()
+
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    header = "Validation: "
+    for it, batch in enumerate(metric_logger.log_every(data_loader, 10, header)):
+
+        o_curr, o_next, o_goal, actions, amask = batch
+
+        o_curr = o_curr.cuda(non_blocking=True)
+        o_next = o_next.cuda(non_blocking=True)
+        o_goal = o_goal.cuda(non_blocking=True) if args.core == "ilpo" else None
+        actions = actions.cuda(non_blocking=True)
+        amask = amask.cuda(non_blocking=True)
+
+        # pass through encoder and decoder and compute recons loss
+        with torch.cuda.amp.autocast(fp16_scaler is not None) and torch.no_grad():
+
+            # encoder outputs quantized latents and quantization loss
+            x_next_pred, _, z_curr, z_reg_loss, x_reg_loss = encoder(o_curr, o_next, o_goal)
+
+            # reconstruct
+            o_next_pred = decoder(x_next_pred)
+            actions_pred = action_decoder(z_curr)
+
+            # accumulate losses
+            rloss = recon_loss(o_next, o_next_pred)
+            aloss = action_loss(actions_pred, actions, amask)
+            z_reg_loss = torch.mean(z_reg_loss)
+
+            loss = rloss + args.beta1 * z_reg_loss + args.beta2 * x_reg_loss
+            if args.alpha != 0:
+                loss += args.alpha * aloss
+
+        if not math.isfinite(loss.item()):
+            print("Loss is {}, stopping training".format(loss.item()), force=True)
+            sys.exit(1)
+
+        # logging
+        torch.cuda.synchronize()
+        metric_logger.update(val_loss=loss.item())
+        metric_logger.update(val_recon_loss=rloss.item())
+        metric_logger.update(val_z_reg_loss=z_reg_loss.item())
+        metric_logger.update(val_x_reg_loss=x_reg_loss.item())
+        metric_logger.update(val_action_loss=aloss.item())
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+
+    stats = {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
+    return stats
 
 
 class ReconLoss(nn.Module):
--- examples/cpt_vqvae.sh
+++ examples/cpt_vqvae.sh
@@ -9,15 +9,14 @@ else
 fi
 
 
+# --data_path $PROJDIR/data/ssv2/20bn-something-something-v2-frames-tiny \
+
 rm -rf $OUTDIR
 python $PROJDIR/CPT/cpt/main_cpt_vqvae.py \
     --encoder_arch vit_tiny \
-    # --data_path $PROJDIR/data/ours/ours_tabletop/ours_moveT_robot_frames \
-    --data_path $PROJDIR/data/ours/ssv2/20bn-something-something-v2-frames-tiny \
+    --data_path $PROJDIR/data/ours/ours_tabletop/ours_moveT_robot_frames \
     --output_dir $OUTDIR \
     --beta1 0.0001 \
     --beta2 0.0001 \
     --alpha 0 \
     --core lapo
-
-
