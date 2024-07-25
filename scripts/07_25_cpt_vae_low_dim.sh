b52a09dba3b9705d59a71bb750e9a39b624ecc80
Gagan Khandate <gagank@cs.columbia.edu>
2024-07-25 19:19:05 +0000

    Training script for ssv2 tiny

---
+++ scripts/07_25_cpt_vae_low_dim.sh
@@ -0,0 +1,31 @@
+RUNDIR="$PROJDIR/runs/ssv2/"
+
+if [ -z "$1" ]
+    then
+    echo "Output directory argument not provided"
+else
+    OUTDIR=$RUNDIR/$1
+fi
+
+gpu=0
+for core in ilpo;  do
+    for lsdim in 16 12 8 4; do
+        for ladim in 3; do
+            for gc in True False; do
+                export CUDA_VISIBLE_DEVICES=$gpu
+                python $PROJDIR/CPT/cpt/main_cpt_vqvae.py \
+                    --encoder_arch vit_tiny \
+                    --data_path $PROJDIR/data/ssv2/20bn-something-something-v2-frames-tiny \
+                    --output_dir $OUTDIR-$core-gc-$gc-lsdim-$lsdim-ladim-$ladim\
+                    --beta1 0.0001 \
+                    --beta2 0.0001 \
+                    --latent_action_dim $ladim \
+                    --alpha 0 \
+                    --core $core \
+                    --epochs 200 \
+                    --skip_frames 5 \
+                    --goal_cond $gc &
+                gpu=$((gpu + 1))
+            done
+        done
+    done
