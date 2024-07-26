cfee88ec1c3f649d8d22c1d65866259d66152444
Gagan Khandate <gagank@cs.columbia.edu>
2024-07-26 18:50:37 +0000

    Scripts to rerun validation experiments

--- scripts/07_25_cpt_val_loss_tabletop.sh
+++ scripts/07_25_cpt_val_loss_tabletop.sh
@@ -1,4 +1,4 @@
-RUNDIR="$PROJDIR/runs/ssv2/"
+RUNDIR="$PROJDIR/runs/tabletop/"
 
 if [ -z "$1" ]
     then
@@ -8,7 +8,7 @@ else
     OUTDIR=$RUNDIR/$1
 fi
 
-gpu=1
+gpu=2
 
 for lsdim in 16 8; do
     for ladim in 3; do
@@ -17,14 +17,14 @@ for lsdim in 16 8; do
                 export CUDA_VISIBLE_DEVICES=$gpu
                 python $PROJDIR/CPT/cpt/main_cpt_vqvae.py \
                     --encoder_arch vit_tiny \
-                    --data_path $PROJDIR/data/ssv2/20bn-something-something-v2-frames-tiny \
+                    --data_path $PROJDIR/data/ours/ours_tabletop/ours_moveT_robot_frames \
                     --output_dir $OUTDIR-$core-gc-$gc-lsdim-$lsdim-ladim-$ladim\
                     --beta1 0.0001 \
                     --beta2 0.0001 \
                     --latent_action_dim $ladim \
                     --alpha 0 \
                     --core $core \
-                    --epochs 200 \
+                    --epochs 100 \
                     --skip_frames 5 \
                     --goal_cond $gc
                 # gpu=$((gpu + 1))
