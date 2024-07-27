b82f9569e51485e3a97da97a89d427a54415053c
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-07-27 23:12:29 +0000

    fixed the train test split

--- data/datasets.py
+++ data/datasets.py
@@ -42,9 +42,6 @@ def load_dataset(
 
     # print(f"Number of image directories: {len(img_dirs)}")
 
-    train_dirs = img_dirs[: int(train_split * len(img_dirs))]
-    val_dirs = img_dirs[int(train_split * len(img_dirs)) :]
-
     kwargs = dict()
     for param in ["skip_frames", "action_only", "use_ee", "context_len", "action_chunk_len"]:
         if hasattr(args, param):
@@ -52,12 +49,14 @@ def load_dataset(
 
     if wrapper_cls == "VisDemoDataset":
         dataset = partial(VisDemoDataset, data_root=data_root, transform=transform, **kwargs)
-        train_dataset = dataset(demo_dirs=train_dirs)
-        val_dataset = dataset(demo_dirs=val_dirs)
     elif wrapper_cls == "SeqVisDemoDataset":
         dataset = partial(SeqVisDemoDataset, data_root=data_root, transform=transform, **kwargs)
-        train_dataset = dataset(demo_dirs=train_dirs)
-        val_dataset = dataset(demo_dirs=val_dirs)
+    full_dataset = dataset(demo_dirs=img_dirs)
+
+    train_size = int(train_split * len(full_dataset))
+    val_size = len(full_dataset) - train_size
+
+    train_dataset, val_dataset = torch.utils.data.random_split(full_dataset, [train_size, val_size])
 
     return train_dataset, val_dataset
 
