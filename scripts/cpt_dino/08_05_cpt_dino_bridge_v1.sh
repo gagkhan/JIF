e3753ac1343f07ba4fa59fc6c887f51552d90c1c
Gagan <gagank@cs.columbia.edu>
2024-08-06 17:59:49 -0400

    Adding scripts

---
+++ scripts/cpt_dino/08_05_cpt_dino_bridge_v1.sh
@@ -0,0 +1,33 @@
+RUNDIR="$PROJDIR/runs/bridgev1/"
+
+if [ -z "$1" ]
+    then
+    echo "Output directory argument not provided"
+    exit 1
+else
+    OUTDIR=$RUNDIR/$1
+fi
+
+core=lapo
+gc=True
+
+NUM_TRAINERS=8
+torchrun \
+    --standalone \
+    --nnodes=1 \
+    --nproc-per-node=$NUM_TRAINERS \
+    $PROJDIR/CPT/cpt/main_cpt_dino.py \
+    --encoder_arch vit_small \
+    --data_path $PROJDIR/data/bridge/raw/bridge_data_v1  \
+    --output_dir $OUTDIR/$core-gc-$gc \
+    --beta1 0.0001 \
+    --beta2 0.0001 \
+    --latent_action_dim 3 \
+    --latent_state_dim 16 \
+    --alpha 0 \
+    --core $core \
+    --epochs 100 \
+    --skip_frames 5 \
+    --goal_cond $gc \
+    --batch_size_per_gpu 64 \
+    --train_split 0.95
