60743b996da7f1199d79104108c4f13a1f911a3f
Gagan Khandate <gagank@cs.columbia.edu>
2024-08-11 01:07:56 +0000

    Scripts for kitchen bridge data

--- scripts/cpt_vae/bridge_v1/08_06_bridge_v1_kitchen_lapo.sh
+++ scripts/cpt_vae/bridge_v1/08_06_bridge_v1_kitchen_lapo.sh
@@ -13,13 +13,13 @@ for core in lapo ilpo ; do
     export CUDA_VISIBLE_DEVICES=$gpu 
     python $PROJDIR/CPT/cpt/main_cpt_vqvae.py \
         --encoder_arch vit_small \
-        --data_path $PROJDIR/data/toykitchen/toykitchen2 \
+        --data_path $PROJDIR/data/toykitchen/ \
         --output_dir $OUTDIR/core-$core \
         --beta1 0.0001 \
         --beta2 0.0001 \
         --alpha 0 \
-        --core lapo \
-        --batch_size_per_gpu 128 \
+        --core $core \
+        --batch_size_per_gpu 256 \
         --train_split 0.95 \
         --num_workers 32 \
         --warmup_epochs 0
