2778b51c03c1f8d905861ba26fd328103d64d461
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-08-15 17:37:06 +0000

    cleanup

--- bet/main_bet.py
+++ bet/main_bet.py
@@ -138,7 +138,6 @@ def train_bet(args):
         fp16_scaler = torch.cuda.amp.GradScaler()
 
     # ============ init schedulers ... ============
-    '''
     lr_schedule = utils.cosine_scheduler(
         args.lr * (args.batch_size_per_gpu * utils.get_world_size()) / 256.0,  # linear scaling rule
         args.min_lr,
@@ -152,19 +151,6 @@ def train_bet(args):
         args.epochs,
         len(data_loader),
     )
-    '''
-    lr_schedule = utils.cosine_scheduler(
-        args.lr,
-        args.min_lr,
-        args.epochs,
-        len(data_loader),
-    )
-    wd_schedule = utils.constant_scheduler(
-        args.weight_decay,
-        args.epochs,
-        len(data_loader),
-    )
-
     print(f"Loss, optimizer and schedulers ready.")
 
     # ============ optionally resume training ... ============
