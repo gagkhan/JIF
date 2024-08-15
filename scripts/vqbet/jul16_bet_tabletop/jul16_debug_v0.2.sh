0ccf465dd373a359dd0e134be2ddd14353f78f8f
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-08-15 15:07:07 +0000

    cleanup

---
+++ scripts/vqbet/jul16_bet_tabletop/jul16_debug_v0.2.sh
@@ -0,0 +1,21 @@
+RUNDIR="/ssd01/gagan/cpt_checkpoints"
+
+if [ -z "$1" ]
+    then
+    echo "Output directory argument not provided"
+else
+    OUTDIR=$RUNDIR/$1
+fi
+
+python ../../../bc/main_bc.py \
+    --decoder_arch mlp_large \
+    --encoder_arch resnet34 \
+    --pretrained_weights IMAGENET1K_V1 \
+    --data_path /ssd01/gagan/cpt_data/ours/jun09_bc_tabletop_rgb \
+    --use_ee \
+    --naug 1 \
+    --batch_size_per_gpu 32 \
+    --epochs 100 \
+    --output_dir $OUTDIR \
+    --lr 0.001 \
+    --alpha 10 \
