50c2681c228f8a38dd5788674b7530b5a6c758eb
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-08-20 15:40:30 +0000

    new script

---
+++ scripts/cpt_dino/08_19_cpt_dino_tabletop/08_19_cpt_dino_tabletop_v0.2.sh
@@ -0,0 +1,21 @@
+RUNDIR="/ssd01/gagan/cpt_checkpoints"
+
+if [ -z "$1" ]
+    then
+    echo "Output directory argument not provided"
+    OUTDIR=$RUNDIR/debug
+else
+    OUTDIR=$RUNDIR/$1
+fi
+
+rm -rf $OUTDIR
+python ../../../cpt/main_cpt_vitact_dino.py \
+    --encoder_arch vitact_small \
+    --data_path /ssd01/gagan/cpt_data/ours/aug09_pickhuman \
+    --output_dir $OUTDIR \
+    --alpha 0.0001 \
+    --beta1 0.0001 \
+    --beta2 0.0001 \
+    --lr 0.001 \
+    --core lapo \
+    --batch_size_per_gpu 64
