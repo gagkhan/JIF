cf83c25c6dbeea090301e6d14291ba994ab3eb5b
Gagan Khandate <gagank@cs.columbia.edu>
2024-10-06 21:26:58 +0000

    add scripts

---
+++ scripts/cpt_vitact_dino/09_06_lapo_skip_10_quant_true.sh
@@ -0,0 +1,22 @@
+RUNDIR="$PROJDIR/runs/cpt_vitact_dino"
+
+if [ -z "$1" ]
+    then
+    echo "Output directory argument not provided"
+    OUTDIR=$RUNDIR/debug
+else
+    OUTDIR=$RUNDIR/$1
+fi
+
+rm -rf $OUTDIR
+python $PROJDIR/CPT/cpt/main_cpt_vitact_dino.py \
+    --encoder_arch vitact_tiny \
+    --data_path $PROJDIR/data/ours/aug09_pickhuman \
+    --output_dir $OUTDIR \
+    --beta1 0.0001 \
+    --beta2 0.0001 \
+    --alpha 0 \
+    --core lapo \
+    --batch_size_per_gpu 64 \
+    --quantize_action True \
+    --skip_frames 10
