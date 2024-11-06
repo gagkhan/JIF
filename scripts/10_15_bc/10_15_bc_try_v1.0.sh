a14ea1febc5d5a660bb143a888f3763433a13c4f
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-11-06 22:39:58 +0000

    added bash scripts

---
+++ scripts/10_15_bc/10_15_bc_try_v1.0.sh
@@ -0,0 +1,23 @@
+RUNDIR="/ssd01/gagan/cpt_checkpoints"
+
+if [ -z "$1" ]
+    then
+    echo "Output directory argument not provided"
+else
+    OUTDIR=$RUNDIR/$1
+fi
+
+python ../../cpt/main_cpt_pure_bc.py \
+    --data_path /ssd01/gagan/cpt_data/ours/10_15_pickrobot \
+    --output_dir $OUTDIR \
+    --use_cam2    False \
+    --use_cam3    True \
+    --use_tactile False \
+    --use_ee      True \
+    --goal_cond   True \
+    --batch_size_per_gpu 32 \
+    --beta 0.0 \
+    --alpha 1.0 \
+    --epochs 100 \
+    --encoder_arch vitact_tiny \
+    --train_split 0.7 \
