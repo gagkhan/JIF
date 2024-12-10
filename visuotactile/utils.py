72c168fa934e4e99a420a1f8cec57f9644d5aa70
Boxuan (Dave) Wang <vulpes.wang@gmail.com>
2024-12-10 03:26:21 +0000

    added replace_bn_with_gn in build_vitact

--- visuotactile/utils.py
+++ visuotactile/utils.py
@@ -1,5 +1,5 @@
 import os
-from typing import Tuple
+from typing import Tuple, Callable
 
 import torch
 import visuotactile.visuo_tactile_transformer as vitact
@@ -31,6 +31,11 @@ def build_vitact_encoder(args) -> Tuple[nn.Module, int]:
         embed_dim = encoder.embed_dim
     else:
         print(f"Unknow architecture: {args.encoder_arch}")
+
+    # IMPORTANT!
+    # replace all BatchNorm with GroupNorm to work with EMA
+    # performance will tank if you forget to do this!
+    encoder = replace_bn_with_gn(encoder)
 
     '''
     # Load pretrained weights
@@ -65,3 +70,56 @@ def build_vitact_encoder(args) -> Tuple[nn.Module, int]:
     '''
 
     return encoder, embed_dim
+
+
+def replace_submodules(
+        root_module: nn.Module,
+        predicate: Callable[[nn.Module], bool],
+        func: Callable[[nn.Module], nn.Module]) -> nn.Module:
+    """
+    Replace all submodules selected by the predicate with
+    the output of func.
+
+    predicate: Return true if the module is to be replaced.
+    func: Return new module to use.
+    """
+    if predicate(root_module):
+        return func(root_module)
+
+    bn_list = [k.split('.') for k, m
+        in root_module.named_modules(remove_duplicate=True)
+        if predicate(m)]
+    for *parent, k in bn_list:
+        parent_module = root_module
+        if len(parent) > 0:
+            parent_module = root_module.get_submodule('.'.join(parent))
+        if isinstance(parent_module, nn.Sequential):
+            src_module = parent_module[int(k)]
+        else:
+            src_module = getattr(parent_module, k)
+        tgt_module = func(src_module)
+        if isinstance(parent_module, nn.Sequential):
+            parent_module[int(k)] = tgt_module
+        else:
+            setattr(parent_module, k, tgt_module)
+    # verify that all modules are replaced
+    bn_list = [k.split('.') for k, m
+        in root_module.named_modules(remove_duplicate=True)
+        if predicate(m)]
+    assert len(bn_list) == 0
+    return root_module
+
+def replace_bn_with_gn(
+    root_module: nn.Module,
+    features_per_group: int=16) -> nn.Module:
+    """
+    Relace all BatchNorm layers with GroupNorm.
+    """
+    replace_submodules(
+        root_module=root_module,
+        predicate=lambda x: isinstance(x, nn.BatchNorm2d),
+        func=lambda x: nn.GroupNorm(
+            num_groups=x.num_features//features_per_group,
+            num_channels=x.num_features)
+    )
+    return root_module
